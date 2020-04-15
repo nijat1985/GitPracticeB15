fb35b2c9defcc4bac49e5e1050a5fde4efda9610
nijat1985 <mehdiyevnicat1985@gmail.com>
2020-04-15 18:08:01 -0400

    dd

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -3,5 +3,10 @@ public class GitPractice {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+
     }
 }
