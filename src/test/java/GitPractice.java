b1afd90bf05951dfe28a13c2734c458f6ff81e8b
nijat1985 <57869658+nijat1985@users.noreply.github.com>
2020-04-15 18:06:44 -0400

    Update GitPractice.java

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -3,5 +3,7 @@ public class GitPractice {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        
+        System.out.println("I like Java");
     }
 }
